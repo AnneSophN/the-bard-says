8e25704a79715e6f2e027854a16c77c99ffe8c9b
heberaguilar <heber.aguilar@akeneo.com>
2023-01-23 18:02:36 +0100

    Commit main.py and story.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from story import get_the_shortest_story_ever
+
+story = get_the_shortest_story_ever()
+
+print('\nA very short story:\n')
+print(story)
---
+++ story.py
@@ -0,0 +1,8 @@
+def get_the_shortest_story_ever():
+
+    return '''
+For sale:
+  baby shoes,
+  never used...
+                  
+by Ernest Hemingway.'''
